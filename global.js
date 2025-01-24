8ff9bedc6964d58866236d2856ae81aeb30abc43
Pansy Kuang <kuangpansy@gmail.com>
2025-01-24 15:25:12 -0800

    lab 3

--- global.js
+++ global.js
@@ -3,14 +3,15 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+  
+let nav = document.createElement('nav');
+  document.body.prepend(nav);
+  
+const ARE_WE_HOME = document.documentElement.classList.contains('home');
 
-// const navLinks = $$("nav a");
-
-// let currentLink = navLinks.find(
-//     (a) => a.host === location.host && a.pathname === location.pathname
-//   );
-
-// currentLink?.classList.add('current');
+const BASE_PATH = window.location.pathname.includes('/dsc106-portfolio/')
+  ? '/dsc106-portfolio/'
+  : '/';
 
 let pages = [
     { url: '', title: 'Home' },
@@ -19,35 +20,34 @@ let pages = [
     { url: 'resume.html', title: 'Resume' }
   ];
   
-let nav = document.createElement('nav');
-  document.body.prepend(nav);
-  
-const ARE_WE_HOME = document.documentElement.classList.contains('home');
-  
 
 for (let p of pages) {
-let url = p.url;
-let title = p.title;
+    let url = p.url;
+    let title = p.title;
 
 
-if (!ARE_WE_HOME && !url.startsWith('http')) {
-    url = '../' + url;
-}
+    // if (!ARE_WE_HOME && !url.startsWith('http')) {
+    //     url = '../' + url;
+    // }
 
-let a = document.createElement('a');
-a.href = url;
-a.textContent = title;
+    if (!url.startsWith('http')) {
+        url = BASE_PATH + url;
+      }
 
-a.classList.toggle(
-    'current',
-    a.host === location.host && a.pathname === location.pathname
-    );
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
 
-    if (a.host !== location.host) {
-    a.target = '_blank';
-    }
+    a.classList.toggle(
+        'current',
+        a.host === location.host && a.pathname === location.pathname
+        );
 
-nav.append(a);
+        if (a.host !== location.host) {
+        a.target = '_blank';
+        }
+
+    nav.append(a);
 
 
 }
